6f3356692abac40c5c252d5f33991d4f49d542c1
javagalbhanu <javagalbhanu@gmail.com>
2017-05-21 17:03:12 +0530

    Draft copy

---
+++ src/qsp/Demo.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testA(){
+		Reporter.log("Open the browser",true);
+		Reporter.log("Enter the URL",true);
+		Reporter.log("Test the app",true);
+		Reporter.log("Close the browser",true);
+	}
+}
